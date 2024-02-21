f5cfb13a4b46981af5377087bd7eb3cb7b0baef2
Alexei Voloshyn <75684845+aleksvoloshyn@users.noreply.github.com>
2024-02-21 18:54:43 +0200

    daynames added

--- src/components/WeatherHubWeekForecast/WeatherHubWeekForecast.jsx
+++ src/components/WeatherHubWeekForecast/WeatherHubWeekForecast.jsx
@@ -1,39 +1,53 @@
 import img from './../../images/w.jpg'
 import css from './weatherHubWeekForecast.module.scss'
-// const weekforecast = [
-//   { id: 1, day: 'Monday', img: img, degree: '28/21' },
-//   { id: 2, day: 'Tuesday', img: img, degree: '28/21' },
-//   { id: 3, day: 'Wednesday', img: img, degree: '28/21' },
-//   { id: 4, day: 'Thursday', img: img, degree: '28/21' },
-//   { id: 5, day: 'Friday', img: img, degree: '28/21' },
-//   { id: 6, day: 'Saturday', img: img, degree: '28/21' },
-//   { id: 7, day: 'Sunday', img: img, degree: '28/21' },
-// ]
+
 const WeatherHubWeekForecast = ({ weekforecast }) => {
+  const daysOfWeek = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ]
+
   return (
     <div className={css.weatherHubWeekForecast}>
       <h2 className={css.weatherHubWeekForecast__title}>Week</h2>
       <ul className={css.weatherHubWeekForecast__list}>
         {/* take first seven day from trip */}
-        {weekforecast.slice(0, 7).map((d, ind) => {
-          return (
-            <li key={ind} className={css.weatherHubWeekForecast__item}>
-              <p className={css.weatherHubWeekForecast__date}>{d.datetime}</p>
-              <img
-                src={`./images/icons/${d.icon}.svg`}
-                alt={d.icon}
-                className={css.weatherHubWeekForecast__icon}
-              />
-              <span className={css.weatherHubWeekForecast__maxmin}>
-                {' '}
-                <p className={css.weatherHubWeekForecast__degr}>
-                  {d.tempmin}&deg;
+        {weekforecast ? (
+          weekforecast.slice(0, 7).map((d, ind) => {
+            const date = new Date(d.datetime)
+            const dayOfWeek = date.getDay()
+            daysOfWeek[dayOfWeek]
+            return (
+              <li key={ind} className={css.weatherHubWeekForecast__item}>
+                <p className={css.weatherHubWeekForecast__date}>
+                  {daysOfWeek[dayOfWeek]}
                 </p>
-                /<p className={css.weatherHubWeekForecast__degr}>{d.tempmax}</p>
-              </span>
-            </li>
-          )
-        })}
+                <img
+                  src={`./images/icons/${d.icon}.svg`}
+                  alt={d.icon}
+                  className={css.weatherHubWeekForecast__icon}
+                />
+                <span className={css.weatherHubWeekForecast__maxmin}>
+                  {' '}
+                  <p className={css.weatherHubWeekForecast__degr}>
+                    {d.tempmin}&deg;
+                  </p>
+                  /
+                  <p className={css.weatherHubWeekForecast__degr}>
+                    {d.tempmax}
+                  </p>
+                </span>
+              </li>
+            )
+          })
+        ) : (
+          <div>loading...</div>
+        )}
       </ul>
     </div>
   )
